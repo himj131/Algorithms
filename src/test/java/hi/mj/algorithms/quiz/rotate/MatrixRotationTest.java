c0f0bd3521e3e70a38c09cdb8034f7df4d3cd45f
star <himj131@gmail.com>
2021-04-07 22:16:20 +0900

    feat: 2차원 배열 90도 회전 시키기

--- src/test/java/hi/mj/algorithms/quiz/rotate/MatrixRotationTest.java
+++ src/test/java/hi/mj/algorithms/quiz/rotate/MatrixRotationTest.java
@@ -2,10 +2,15 @@ package hi.mj.algorithms.quiz.rotate;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * https://leetcode.com/problems/rotate-image/
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
  *
  * Input: matrix = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}}
  * Output: {{15,13,2,5},{14,3,4,1},{12,6,8,9},{16,7,10,11}}
@@ -32,25 +37,22 @@ class MatrixRotationTest {
     }
 
     private int[][] rotate(int[][] matrix) {
-        int[][] result = null;
-        for(int i = 0; i < matrix.length; i++) {
-//            for(int j = 0; i < matrix.length; i++) {
-//                int first = matrix[i][j];
-//                int second = matrix[i][j];
-//            }
+        int lastIdx = matrix.length - 1;
+        int length = matrix.length;
 
-
+        Map<Integer, int[]> temp = new HashMap<>();
+        for(int i = 0; i < length; i++) {
+            int[] num = matrix[i].clone();
+            temp.put(i, num);
         }
 
-        //matrix[0][0] -> matrix[0][3]
-        //matrix[0][1] -> matrix[1][3]
-        //matrix[0][2] -> matrix[2][3]
-        //matrix[0][3] -> matrix[3][3]
-        //matrix[0][0] -> matrix[0][3]
-        //matrix[1][0] -> matrix[0][2]
-        //matrix[1][2] -> matrix[2][3]
-        //matrix[0][3] -> matrix[3][3]
-        return result;
+        for(int i = 0; i < length ; i++) {
+            for(int j = 0; j < length ; j++) {
+              matrix[j][lastIdx - i] = temp.get(i)[j];
+            }
+        }
+
+        return matrix;
     }
 }
 
